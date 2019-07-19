11a7d963ce73125296d3aa4cb4dec1c143769334
erinpc <4540807@qq.com>
2019-07-19 09:59:31 +0800

    init

---
+++ testcase/test_requests.py
@@ -0,0 +1,17 @@
+# !/user/bin/env python
+# coding:utf-8
+import requests
+import logging
+import pytest
+import json
+
+
+class TestRequests():
+    logging.basicConfig(level=logging.INFO)
+    def test_get(self):
+        r = requests.get("https://testerhome.com/api/v3/topics.json?limit=2")
+        logging.info(r)
+        logging.info(r.text)
+        logging.info(r.json())
+        logging.info('------')
+        logging.info(json.dumps(r.json(),indent=2))
